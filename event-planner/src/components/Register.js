da13211caef21ab5f158c1b6538d1a6fbedc3f02
NV1028 <51763199+NV1028@users.noreply.github.com>
2019-11-17 20:44:35 -0600

    styled register form added

--- event-planner/src/components/Register.js
+++ event-planner/src/components/Register.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-import axiosAuth from "../utils/axiosAuth"
-import { Link } from 'react-router-dom'
+import axiosAuth from "../utils/axiosAuth";
+import { Link } from "react-router-dom";
 
-const Register = (props) => {
-  const [data, setData] = useState({ 
-    "email": "",
-    "password": "",
-    "role": "",
-    "company": ""
-    });
+const Register = props => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    role: "",
+    company: ""
+  });
 
   const handleChange = e => {
     console.log(e);
@@ -20,55 +20,74 @@ const Register = (props) => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(data)
+    console.log(data);
     axiosAuth()
-      .post('/api/auth/register', data)
+      .post("/api/auth/register", data)
       .then(res => {
-        console.log(res.data)
-        localStorage.setItem('token', res.data.token)
-        props.history.push('/user');
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token);
+        props.history.push("/user");
       })
       .catch(err => console.log(err));
   };
 
   return (
     <>
-      <h3>Please register</h3>
-  <div><Link to={"/"}>Home </Link></div>
-      <form onSubmit={handleSubmit}>
-        <input
-          name='email'
-          type='email'
-          placeholder='Email'
-          value={data.email}
-          onChange={e => handleChange(e)}
-        />
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={data.password}
-          onChange={e => handleChange(e)}
-        />
-         <input
-          name='role'
-          type='test'
-          placeholder='role'
-          value={data.role}
-          onChange={e => handleChange(e)}
-        />
-        <input
-          name='company'
-          type='text'
-          placeholder='company'
-          value={data.company}
-          onChange={e => handleChange(e)}
-        />
-        <button type='submit'>Register</button>
-      </form>
+      <div className="FormCenter">
+        <h3>Please register</h3>
+        <div>
+          <Link to={"/"}>Home </Link>
+        </div>
+        <form onSubmit={handleSubmit} className="RegisterForm">
+          <br />
+
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={data.email}
+            className="formInput"
+            onChange={e => handleChange(e)}
+          />
+          <br />
+          <br />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={data.password}
+            className="formInput"
+            onChange={e => handleChange(e)}
+          />
+          <br />
+          <br />
+          <input
+            name="role"
+            type="test"
+            placeholder="Role"
+            value={data.role}
+            className="formInput"
+            onChange={e => handleChange(e)}
+          />
+          <br />
+          <br />
+          <input
+            name="company"
+            type="text"
+            placeholder="Company"
+            value={data.company}
+            className="formInput"
+            onChange={e => handleChange(e)}
+          />
+          <br />
+          <br />
+          <button id="login-button" className="submit-button" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
     </>
   );
 };
 
-
-export default Register
+export default Register;

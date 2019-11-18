850f9eaa54156ba292c29d0510171b983e2e0c22
josegocampo <jose.ocampo@mail.udp.cl>
2019-11-17 21:04:00 -0300

    my chnges

--- event-planner/src/components/Card.js
+++ event-planner/src/components/Card.js
@@ -1,10 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const Cards= styled.div`
+    background: #fff;
+    box-shadow: 0 0 32px rgba(0,0,0,0.16);
+    border-radius: 8px;
+    display: inline-block;
+    margin-bottom: 32px;
+    margin-right: 30px;
+    min-height: 350px;
+    position: relative;
+    vertical-align: top;
+    width: 300px;
+`
+
+
+
+
 
 const Card = (props) => {
 return (
-    <div>
-      <h2>Card Component Rendered</h2>
-    </div>
+    <Cards>
+      <h2>{props.name}</h2>
+      <img src= {props.image} alt ="image of char"/>
+    </Cards>
   )
 }
 

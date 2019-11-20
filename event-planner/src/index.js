7a521a2e3e39756c555f23e3a3b86c0ccab11228
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2019-11-19 19:13:38 -0600

    -Changed index.js Router and Provider wrapping.

--- event-planner/src/index.js
+++ event-planner/src/index.js
@@ -18,11 +18,11 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>, 
+    <Router>
+        <Provider store={store}>    
+         <App />  
+        </Provider>
+    </Router>, 
     document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

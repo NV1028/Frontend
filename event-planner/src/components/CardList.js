4542096971242bf2e75167915f7d70e03cf1ed7a
NV1028 <51763199+NV1028@users.noreply.github.com>
2019-11-21 18:22:32 -0600

    styled login button

--- event-planner/src/components/CardList.js
+++ event-planner/src/components/CardList.js
@@ -7,45 +7,41 @@ import { fetchEvents } from "../actions";
 const CardHolder = styled.div`
   display: flex;
   flex-direction: row;
-  width: 1200px;
   flex-wrap: wrap;
   margin: 0 auto;
   justify-content: center;
   margin-top: 50px;
+  margin-bottom: 100px;
 `;
 
 const CardList = props => {
-  const [events, setEvents] = useState([]);
   useEffect(() => {
     props.fetchEvents(props.login.userId);
-    console.log("events");
-    console.log(props.fetchEvents.eventList);
-    setEvents(props.fetchEvents.eventList);
   }, []);
 
   return (
     <CardHolder>
-      {/* {events.map(info => (
-  <Card event_name={info.event_name} description={info.description} budget={info.budget} event_date={info.event_date} event_time={info.event_time} />
-))} */}
+      {props.events.eventsList &&
+        props.events.eventsList.map(info => (
+          <Card
+            event_name={info.event_name}
+            description={info.description}
+            budget={info.budget}
+            event_date={info.event_date}
+            event_time={info.event_time}
+          />
+        ))}
     </CardHolder>
   );
 };
 
 function mapStateToProps(state) {
+  console.log(state);
   return {
     ...state
   };
 }
 
+
 export default connect(mapStateToProps, { fetchEvents })(CardList);
 
-// return (
-//     <CardHolder>
-
-// {data.map(info => (
-//            <Card event_name={info.name} description={info.species} budget={info.status} event_date={info.gender} event_time={info.id} image={info.image} />
-//         ))}
-//     </CardHolder>
-//   )
-// }
